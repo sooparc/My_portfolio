64126154a13aa9d223c328df019703d83b66d0cf
Soojin Park <soozin95@gmail.com>
2022-11-03 13:09:52 -0700

    style changed

--- src/Data/projectData.js
+++ src/Data/projectData.js
@@ -1,17 +1,17 @@
 export default [
   {
     id: "1",
-    name: "PT FINDER",
+    name: "SIMPLE JOBS",
     technologies: "React JS, MYSQL, Node JS, HTML, CSS",
     description: "Looking for part time jobs? Quick apply",
-    url: "https://magenta-boba-e9d4bc.netlify.app",
+    url: "https://thriving-gnome-8f51ab.netlify.app/",
   },
   {
     id: "2",
     name: "AMAZING BREWERIES",
     technologies: "React JS, Firebase, HTML, CSS",
     description: "Check out the best breweries in LA",
-    url: "https://breweries-app-ab262.web.app",
+    url: "https://breweries-app-ab262.web.app/",
   },
   {
     id: "3",
@@ -22,23 +22,23 @@ export default [
   },
   {
     id: "4",
-    name: "ALL RECIPES",
-    technologies: "EJS, Node JS, Mongo DB, HTML, CSS",
-    description: "Find meals for your ingredients",
-    url: "https://calm-reaches-31420.herokuapp.com/",
+    name: "UNION NAIL SALON",
+    technologies: "React JS, Firebase, HTML, CSS",
+    description: "Book an appointment with a manicurist you like",
+    url: "https://unionnailsalon-app.web.app",
   },
   {
     id: "5",
-    name: "AFFORABLE PIZZA",
+    name: "MONSTER PIZZA",
     technologies: "React JS, Firebase, HTML, CSS",
     description: "Try ordering delicious and afforable pizza",
     url: "https://pizzaordering-app.web.app",
   },
   {
     id: "6",
-    name: "GREAT QUOTES",
-    technologies: "React JS, Firebase, HTML, CSS",
-    description: "Add your favorite quotes",
-    url: "https://quoteapp-6bfb0.web.app/quotes",
+    name: "ALL RECIPES",
+    technologies: "EJS, Node JS, Mongo DB, HTML, CSS",
+    description: "Find meals for your ingredients",
+    url: "https://calm-reaches-31420.herokuapp.com/",
   },
 ];
--- src/Page/Home.js
+++ src/Page/Home.js
@@ -3,28 +3,33 @@ import classes from "../Page/Home.module.css";
 import projects from "../Data/projectData";
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faFileArrowDown } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { Card } from "react-bootstrap";
-import projectImg1 from "../Assets/PT_Finder_app.png";
+import projectImg1 from "../Assets/Simplejobs.png";
 import projectImg2 from "../Assets/AmazingBreweries.png";
 import projectImg3 from "../Assets/IdealType-app.png";
-import projectImg4 from "../Assets/Recipe-app.png";
+import projectImg4 from "../Assets/UnionNailSalon.png";
 import projectImg5 from "../Assets/pizzaOrdering-app.png";
-import projectImg6 from "../Assets/GreatQuotes-app.png";
+import projectImg6 from "../Assets/Recipe-app.png";
 import skill1 from "../Assets/javascript.png";
 import skill2 from "../Assets/html.jpg";
 import skill3 from "../Assets/css.png";
-import skill4 from "../Assets/reactjs.jpg";
-import skill5 from "../Assets/mysql.png";
-import skill6 from "../Assets/firebase.png";
-import skill7 from "../Assets/nodejs.png";
-import skill8 from "../Assets/mongodb.jpg";
+import skill4 from "../Assets/typeScript.jpeg";
+import skill5 from "../Assets/jQuery.png";
+import skill6 from "../Assets/reactjs.jpg";
+import skill7 from "../Assets/mysql.png";
+import skill8 from "../Assets/firebase.png";
+import skill9 from "../Assets/nodejs.png";
+import skill10 from "../Assets/mongodb.jpg";
+
+import pdf from "../Assets/Soojin_Park_Resume.pdf";
 
 const Home = () => {
   const githubIcon = <FontAwesomeIcon icon={faGithub} />;
   const linkedinIcon = <FontAwesomeIcon icon={faLinkedin} />;
   const gmailIcon = <FontAwesomeIcon icon={faEnvelope} />;
+  const downloadIcon = <FontAwesomeIcon icon={faFileArrowDown} />;
 
   let picArr = [
     projectImg1,
@@ -35,63 +40,72 @@ const Home = () => {
     projectImg6,
   ];
 
-  let skillArr = [
-    skill1,
-    skill2,
-    skill3,
-    skill4,
-    skill5,
-    skill6,
-    skill7,
-    skill8,
-  ];
+  let skillArr = [skill1, skill2, skill3, skill4, skill5];
+
+  let skillArr2 = [skill6, skill7, skill8, skill9, skill10];
 
   return (
     <div className={classes.body}>
       <div className={classes.leftContainer}>
         <div className={classes.leftContent}>
           <img src={profile} className={classes.profileImg} />
-          <div className={classes.introductionTitle}>Hello! I'm Soo Park.</div>
+          <div className={classes.introductionTitle}>Hello! I'm Soo Park</div>
           <div className={classes.introductionContent}>
             <br /> I'm a<span> </span>
             <div className={classes.highlight}>Full Stack Developer</div> living
-            in Los Angeles.
-            <br /> I enjoy building creative & functional websites.
+            in Los Angeles
+            <br /> I enjoy building creative & functional websites
           </div>
 
-          <a href="https://github.com/sooparc" className={classes.icon}>
-            {githubIcon}
-          </a>
-          <a
-            href="https://www.linkedin.com/in/soojin-park-6bb373141/"
-            className={classes.icon}
-          >
-            {linkedinIcon}
-          </a>
-          <a href="mailto:soozin@gmail.com" className={classes.icon}>
-            {gmailIcon}
-          </a>
+          <div className={classes.iconContainer}>
+            <a href="https://github.com/sooparc" className={classes.icon}>
+              {githubIcon}
+            </a>
+            <a
+              href="https://www.linkedin.com/in/soojin-park-6bb373141/"
+              className={classes.icon}
+            >
+              {linkedinIcon}
+            </a>
+            <a href="mailto:soozin@gmail.com" className={classes.icon}>
+              {gmailIcon}
+            </a>
+            <a href={pdf} className={classes.icon}>
+              {downloadIcon}
+            </a>
+            <div className={classes.copyright}>
+              &copy; 2022 SOOJIN PARK | Full Stack Developer
+            </div>
+          </div>
         </div>
       </div>
 
       <div className={classes.rightContainer}>
         <div className={classes.mainContainer}>
-          <div className={classes.mainTitle}>Soo Park ○ 박수진</div>
-          <div className={classes.mainContent}>
-            Hi, my name is Soo Park. Welcome to my journey in full stack web
-            development. I enjoy learning and applying new technologies into my
-            applications. I believe incorporating new ideas and collaborating
-            with different people will lead to new heights and understanding.
-            Please feel free to contact me by email or ping me on Linkedin !
+          <div className={classes.resumeDownload}>
+            <a href={pdf} className={classes.resumeLink}>
+              {downloadIcon}
+              <span> </span> My Resume
+            </a>
           </div>
-          <br />
+          <div className={classes.mainTitle}>About me</div>
           <div className={classes.mainContent}>
-            안녕하세요? 저는 한국에서 온 풀스텍 개발자 박수진입니다. 먼저 제
-            웹사이트를 방문해 주셔서 감사합니다. 일에 관해서는 항상 무엇이든
-            배우려는 자세로 새로운 기술을 제 어플리케이션에 적용하는 것에 열정을
-            가지고 임하고 있습니다. 창의적인 생각과 다양한 사람들과 협업하는
-            것은 폭넓은 관점과 더 나은 이해를 불러오는 것이라고 믿고 있습니다.
-            편히 링크드인이나 이메일로 연락 주시면 감사드리겠습니다 :)
+            A passionate Full Stack Developer having an experience of building
+            websites with{" "}
+            <strong>
+              JavaScript | React.js | TypeScript | jQuery | Node.js | MongoDB |
+              MySQL
+            </strong>{" "}
+            and other libraries and frameworks. I enjoy learning and applying
+            new technologies into my applications. I believe incorporating new
+            ideas and collaborating with different people will lead to new
+            heights and understanding. Please feel free to contact me by{" "}
+            <a href="mailto:soozin@gmail.com">email</a> or ping me on{" "}
+            <span> </span>
+            <a href="https://www.linkedin.com/in/soojin-park-6bb373141/">
+              Linkedin
+            </a>
+            !
           </div>
         </div>
         <hr className={classes.divider} />
@@ -118,13 +132,13 @@ const Home = () => {
             ))}
           </div>
           <hr className={classes.divider} />
-          <div className={classes.skillTitle}>Skills</div>
           <div className={classes.skills}>
-            <div className="row mx-5">
-              {skillArr.map((skill, i) => (
-                <div className="col-md-3 col-sm-6">
-                  <img src={skillArr[i]} className={classes.skill} />
-                </div>
+            {skillArr.map((skill, i) => (
+              <img src={skillArr[i]} className={classes.skill} />
+            ))}
+            <div>
+              {skillArr2.map((skill, i) => (
+                <img src={skillArr2[i]} className={classes.skill} />
               ))}
             </div>
           </div>
