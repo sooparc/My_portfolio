cb5070ac0e66d2ce5cf0fe6a11952849fb3aec6b
Soojin  Park <soozin95@gmail.com>
2025-07-20 12:54:12 -0700

    Initial commit

--- src/Data/projectData.js
+++ src/Data/projectData.js
@@ -1,44 +1,30 @@
 export default [
   {
     id: "0",
-    name: "MEDICLO",
-    technologies: "Javascript, jQuery, Liquid, HTML, CSS",
-    description: "",
-    url: "https://mediclo.com",
-  },
-  {
-    id: "1",
     name: "SIMPLE JOBS",
     technologies: "React JS, MYSQL, Node JS, HTML, CSS",
     description: "Looking for part time jobs? Quick apply",
     url: "https://simplejobs.netlify.app",
   },
   {
-    id: "2",
+    id: "1",
     name: "AMAZING BREWERIES",
     technologies: "React JS, Firebase, HTML, CSS",
     description: "Check out the best breweries in LA",
     url: "https://breweries-app-ab262.web.app/",
   },
   {
-    id: "3",
+    id: "2",
     name: "IDEAL TYPE FINDER",
     technologies: "React JS, Firebase, HTML, CSS",
     description: "Find out which K-pop star is your ideal type",
     url: "https://idealtype-app.web.app/",
   },
   {
-    id: "4",
+    id: "3",
     name: "UNION NAIL SALON",
     technologies: "React JS, Firebase, HTML, CSS",
     description: "Book an appointment with a manicurist you like",
     url: "https://unionnailsalon-app.web.app",
-  },
-  {
-    id: "5",
-    name: "MONSTER PIZZA",
-    technologies: "React JS, Firebase, HTML, CSS",
-    description: "Try ordering delicious and afforable pizza",
-    url: "https://pizzaordering-app.web.app",
   },
 ];
--- src/Page/Home.js
+++ src/Page/Home.js
@@ -6,12 +6,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faFileArrowDown } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { Card } from "react-bootstrap";
-import projectImg0 from "../Assets/Mediclo.png";
+import projectImg0 from "../Assets/Mediclo.jpg";
 import projectImg1 from "../Assets/Simplejobs.png";
 import projectImg2 from "../Assets/AmazingBreweries.png";
 import projectImg3 from "../Assets/IdealType-app.png";
 import projectImg4 from "../Assets/UnionNailSalon.png";
-import projectImg5 from "../Assets/pizzaOrdering-app.png";
+import projectImg6 from "../Assets/betaboulders.jpg"
 import skill1 from "../Assets/javascript.png";
 import skill2 from "../Assets/html.jpg";
 import skill3 from "../Assets/css.png";
@@ -23,7 +23,7 @@ import skill8 from "../Assets/firebase.png";
 import skill9 from "../Assets/nodejs.png";
 import skill10 from "../Assets/mongodb.jpg";
 
-import pdf from "../Assets/Soojin_park_resume.pdf";
+import pdf from "../Assets/Soojin_Park_Resume.pdf";
 
 const Home = () => {
   const githubIcon = <FontAwesomeIcon icon={faGithub} />;
@@ -32,12 +32,10 @@ const Home = () => {
   const downloadIcon = <FontAwesomeIcon icon={faFileArrowDown} />;
 
   let picArr = [
-    projectImg0,
     projectImg1,
     projectImg2,
     projectImg3,
     projectImg4,
-    projectImg5,
   ];
 
   let skillArr = [skill1, skill2, skill3, skill4, skill5];
@@ -45,24 +43,19 @@ const Home = () => {
   let skillArr2 = [skill6, skill7, skill8, skill9, skill10];
 
   return (
-    <div className={classes.body}>
+    <div className={classes.container}>
       <div className={classes.leftContainer}>
         <div className={classes.leftContent}>
           <img src={profile} className={classes.profileImg} />
           <div className={classes.introductionTitle}>Hello! I'm Soo Park</div>
           <div className={classes.introductionContent}>
-            <br /> I'm a<span> </span>
-            <div className={classes.highlight}>Full Stack Web Developer</div> living
-            in Los Angeles
-            <br /> I enjoy building creative & functional websites
+            <span>a Full Stack Web Developer</span> based in Los Angeles with 4 years of experience<br/>
+            building creative & functional web applications.
           </div>
 
           <div className={classes.iconContainer}>
-            <a href="https://github.com/sooparc" className={classes.icon}>
-              {githubIcon}
-            </a>
             <a
-              href="https://www.linkedin.com/in/soojin-park-6bb373141/"
+              href="https://www.linkedin.com/in/soojin-park-dev/"
               className={classes.icon}
             >
               {linkedinIcon}
@@ -70,48 +63,76 @@ const Home = () => {
             <a href="mailto:soozin@gmail.com" className={classes.icon}>
               {gmailIcon}
             </a>
+            <a href="https://github.com/sooparc" className={classes.icon}>
+              {githubIcon}
+            </a>
             <a href={pdf} className={classes.icon}>
               {downloadIcon}
             </a>
             <div className={classes.copyright}>
-              &copy; 2024 SOOJIN PARK | Full Stack Web Developer
+              &copy; 2025 SOOJIN PARK | Full Stack Web Developer
             </div>
           </div>
         </div>
       </div>
 
       <div className={classes.rightContainer}>
-        <div className={classes.mainContainer}>
-          <div className={classes.resumeDownload}>
-            <a href={pdf} className={classes.resumeLink}>
-              {downloadIcon}
-              <span> </span> My Resume
-            </a>
-          </div>
-          <div className={classes.mainTitle}>About me</div>
-          <div className={classes.mainContent}>
-            Full Stack Web Developer with 3 years of experience in building
-            dynamic websites and applications using a variety of technologies, including{" "}
-            <strong>
-              React.js | JavaScript | jQuery | Node.js | MongoDB |
-              MySQL | Liquid | Figma
-            </strong>{" "}
-            Skilled in both front-end and back-end development,
-            with a passion for learning and intergrating new tools and frameworks. 
-            Please feel free to contact me by{" "}
-            <a href="mailto:soozin95@gmail.com">email</a> or ping me on{" "}
-            <span> </span>
-            <a href="https://www.linkedin.com/in/soojin-park-6bb373141/">
-              Linkedin
-            </a>
-            !
+        <div className={classes.resumeDownload}>
+          <a href={pdf} className={classes.resumeLink}>
+            {downloadIcon} <span style={{marginLeft: "5px"}}>My Resume</span>
+          </a>
+        </div>
+        <div className={classes.contentContainer}>
+          <div className={classes.contentHeading}>About me</div>
+          <div className={classes.contentText}>
+            I build seamless, high-performing web applications that solve real business problems. 
+            With expertise in both front-end and back-end development, I create custom features that elevate user experience and meet business goals. 
+            I move fast, adapt quickly, and thrive on turning ideas into functional, optimized solutions. I’m driven by the challenge of building smarter, cleaner, and more impactful web experiences.
           </div>
         </div>
         <hr className={classes.divider} />
 
-        <div className={classes.projectContainer}>
-          <div className={classes.projectTitle}>Projects</div>
-          <div className="row mx-5">
+        <div className={classes.contentContainer}>
+          <div className={classes.contentHeading}>Work</div>
+            <div className={classes.workContainer}>
+              <div className={classes.workImgContainer}>
+                <img src={projectImg0}/>
+              </div>
+            <div className={classes.workTextContainer}>
+              <p className={classes.workHeading}>Mediclo</p>
+              <ul className={classes.customBullets}>
+                <li>Led all technical development and maintenance of this Shopify Plus store.</li>
+                <li>Built custom features like product bundling, embroidery personalization, and solutions that bypass Shopify’s 100-variant limit.</li>
+                <li>Optimized SEO and site performance to improve search visibility and load speed.</li>
+                <li>Integrated third-party apps to expand store functionality while ensuring smooth performance.</li>
+                <li>Managed GA4 and Meta Pixel setup for robust conversion tracking.</li>
+                <li>Created optimized landing pages for email campaigns and Meta ads to boost engagement and drive conversions.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+         <div className={classes.contentContainer}>
+          <div className={classes.workContainer}>
+            <div className={classes.workImgContainer}>
+              <img src={projectImg6}/>
+            </div>
+            <div className={classes.workTextContainer}>
+              <p className={classes.workHeading}>Betaboulders</p>
+              <ul className={classes.customBullets}>
+                <li>Converted a jQuery frontend application to React.js, enhancing maintainability and performance.</li>
+                <li>Developed and tested key Gym Account features, including the Gym Portal, Print Beta Tags, and Route Management, ensuring seamless user experience and functionality.</li>
+                <li>Troubleshot and debugged various production issues to maintain system stability and improve overall application reliability.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <hr className={classes.divider} />
+
+        <div className={classes.contentContainer}>
+          <div className={classes.contentHeading}>Projects</div>
+          <div className="row">
             {projects.map((project, i) => (
               <div className="col-md-6">
                 <Card className={classes.card}>
@@ -130,26 +151,13 @@ const Home = () => {
               </div>
             ))}
           </div>
-          <hr className={classes.divider} />
-          <div className={classes.skills}>
-            {skillArr.map((skill, i) => (
-              <img src={skillArr[i]} className={classes.skill} />
-            ))}
-            <div>
-              {skillArr2.map((skill, i) => (
-                <img src={skillArr2[i]} className={classes.skill} />
-              ))}
-            </div>
-          </div>
         </div>
-        <div
-          class="container mt-5"
-          style={{ fontFamily: "Poppins, sans-serif" }}
-        >
+
+        <hr className={classes.divider} />
           <div className={classes.footer}>
-            <p className="text-end mt-3 text-muted">&copy; 2024 SOOJIN PARK</p>
+            <p className="text-end mt-3 text-muted">&copy; 2025 SOOJIN PARK</p>
           </div>
-        </div>
+  
       </div>
     </div>
   );
